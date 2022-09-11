75000e9960b5a7b5761ca45ec737774fdc0660eb
ilkayus <ilkay.devecioglu@gmail.com>
2022-09-11 22:59:14 +0300

    utils

---
+++ src/util/utils.ts
@@ -0,0 +1,13 @@
+export const postsPerPage = 10;
+export const tableHeaders = [
+  "Crypto",
+  "Symbol",
+  "Image",
+  "Current Price",
+  "Marcet Cap Rank",
+  "Usd Market Cap",
+  "Daily Hight",
+  "Daily Low",
+  "Daily Change",
+  "Daily Change Percentage",
+];
